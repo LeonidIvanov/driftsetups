6beda8aac9eae15202060ad90642f8bcc904e467
LeonidIvanov <git@github.com:LeonidIvanov/python0709.git>
2018-01-26 23:54:42 +0200

    Added og markup in setup_detail template

--- car/models.py
+++ car/models.py
@@ -85,6 +85,10 @@ class CarModel(models.Model):
     def get_sub_models(self):
         return CarSubModel.objects.filter(car_model=self)
 
+    def split_tags(self):
+        car_name = '{0} {1}'.format(self.brand, self.name)
+        return car_name.split(' ')
+
     def get_setups(self):
         setups = chain()
         for car_sub_model in self.get_sub_models():
@@ -121,6 +125,10 @@ class CarSubModel(models.Model):
         self.views += 1
         self.save()
 
+    def split_tags(self):
+        car_name = '{0} {1} {2}'.format(self.car_model.brand, self.car_model, self.name)
+        return car_name.split(' ')
+
     def get_main_image(self):
         return self.images.get(is_main=True)
 
@@ -128,4 +136,4 @@ class CarSubModel(models.Model):
         return reverse('car_sub_model_detail', args=[str(self.car_model.brand.slug), str(self.car_model.slug), str(self.slug)])
 
     def __str__(self):
-        return '{0} {1}'.format(self.car_model, self.name)
+        return '{0} {1} {2}'.format(self.car_model.brand, self.car_model, self.name)
