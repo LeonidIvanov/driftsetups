ef9760efc3d8f4c9bb9ec03bedc98ea8679b20e1
LeonidIvanov <git@github.com:LeonidIvanov/python0709.git>
2018-01-26 11:11:38 +0200

    MEDIA confs

--- driftsetups/settings.py
+++ driftsetups/settings.py
@@ -165,6 +165,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_ROOT = os.environ.get("STATIC_ROOT", os.path.join(os.path.dirname(BASE_DIR), 'staticfiles'))
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
 
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
