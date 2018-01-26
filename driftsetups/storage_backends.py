5aca4ad9b040d815e43ea98cd257a369e664ee45
LeonidIvanov <git@github.com:LeonidIvanov/python0709.git>
2018-01-26 19:14:20 +0200

    AWS S3 fix of files without / in the end

--- driftsetups/storage_backends.py
+++ driftsetups/storage_backends.py
@@ -5,27 +5,27 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class StaticStorage(S3Boto3Storage):
     location = settings.AWS_STATIC_LOCATION
 
-    def _clean_name(self, name):
-        return name
-
-    def _normalize_name(self, name):
-        if not name.endswith('/'):
-            name += "/"
-
-        self.location += name
-        return self.location
+    # def _clean_name(self, name):
+    #     return name
+    #
+    # def _normalize_name(self, name):
+    #     if not name.endswith('/'):
+    #         name += "/"
+    #
+    #     self.location += name
+    #     return self.location
 
 
 class MediaStorage(S3Boto3Storage):
     location = settings.AWS_MEDIA_LOCATION
     file_overwrite = False
 
-    def _clean_name(self, name):
-        return name
-
-    def _normalize_name(self, name):
-        if not name.endswith('/'):
-            name += "/"
-
-        self.location += name
-        return self.location
+    # def _clean_name(self, name):
+    #     return name
+    #
+    # def _normalize_name(self, name):
+    #     if not name.endswith('/'):
+    #         name += "/"
+    #
+    #     self.location += name
+    #     return self.location
