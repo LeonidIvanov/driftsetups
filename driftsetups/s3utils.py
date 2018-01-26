8f8ac663fe69e8fca9d32cc1799de43b29cf0a3b
LeonidIvanov <git@github.com:LeonidIvanov/python0709.git>
2018-01-26 10:54:52 +0200

    MEDIA confs

--- driftsetups/s3utils.py
+++ driftsetups/s3utils.py
@@ -1,21 +1,5 @@
-from storages.backends.s3boto import S3BotoStorage
+from storages.backends.s3boto3 import S3Boto3Storage
 
-
-class StaticS3BotoStorage(S3BotoStorage):
-    """
-    Storage for static files.
-    """
-
-    def __init__(self, *args, **kwargs):
-        kwargs['location'] = 'static'
-        super(StaticS3BotoStorage, self).__init__(*args, **kwargs)
-
-
-class MediaS3BotoStorage(S3BotoStorage):
-    """
-    Storage for uploaded media files.
-    """
-
-    def __init__(self, *args, **kwargs):
-        kwargs['location'] = 'media'
-        super(MediaS3BotoStorage, self).__init__(*args, **kwargs)
+class MediaStorage(S3Boto3Storage):
+    location = 'media'
+    file_overwrite = False
--- driftsetups/settings.py
+++ driftsetups/settings.py
@@ -166,29 +166,26 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.environ.get("STATIC_ROOT", os.path.join(os.path.dirname(BASE_DIR), 'staticfiles'))
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'driftsetups', 'static'),
-]
-
-MEDIA_ROOT = os.path.join("DEFAULT_FILE_STORAGE", os.path.join(os.path.dirname(BASE_DIR), 'media'))
-
-MEDIA_URL = '/media/'
-MEDIAFILES_DIRS = [
-    os.path.join(BASE_DIR, 'driftsetups', 'media'),
-]
-
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-AWS_S3_SECURE_URLS = True      # use http instead of https
-AWS_QUERYSTRING_AUTH = False
-AWS_STORAGE_BUCKET_NAME = "driftsetups"
-AWS_S3_HOST = "s3-us-west-1.amazonaws.com"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'mysite/static'),
+]
 
-STATICFILES_STORAGE = "driftsetups.s3utils.StaticS3BotoStorage"
-DEFAULT_FILE_STORAGE = "driftsetups.s3utils.MediaS3BotoStorage"
+AWS_S3_SECURE_URLS = True
+AWS_STORAGE_BUCKET_NAME = 'driftsetups'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+AWS_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+DEFAULT_FILE_STORAGE = 'driftsetups.s3utils.MediaStorage'
 
 
 # EMAIL
