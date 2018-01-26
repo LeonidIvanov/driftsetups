d3ed6079d106f77e5b83d7c49d2c491c70eaa7ad
LeonidIvanov <git@github.com:LeonidIvanov/python0709.git>
2018-01-26 12:19:26 +0200

    MEDIA confs

--- driftsetups/storage_backends.py
+++ driftsetups/storage_backends.py
@@ -2,6 +2,10 @@ from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class MediaStorage(S3Boto3Storage):
+class StaticS3BotoStorage(S3Boto3Storage):
+    location = 'static'
+
+
+class MediaS3BotoStorage(S3Boto3Storage):
     location = 'media'
     file_overwrite = False
