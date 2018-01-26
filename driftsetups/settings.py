44eff4d5323d8a2a94125a6b6062523c9fe9044c
LeonidIvanov <git@github.com:LeonidIvanov/python0709.git>
2018-01-26 11:15:44 +0200

    MEDIA confs

--- driftsetups/settings.py
+++ driftsetups/settings.py
@@ -183,9 +183,11 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 
-AWS_LOCATION = 'static'
+AWS_STATIC_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
+AWS_MEDIA_LOCATION = 'media'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_MEDIA_LOCATION)
 
 DEFAULT_FILE_STORAGE = 'driftsetups.s3utils.MediaStorage'
 
