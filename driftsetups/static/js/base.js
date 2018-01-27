42d8ca407a06bb0dfa0eaa32e746a55f9ec543bd
LeonidIvanov <git@github.com:LeonidIvanov/python0709.git>
2018-01-27 15:46:53 +0200

    Added Facebook share button functionality

--- driftsetups/static/js/base.js
+++ driftsetups/static/js/base.js
@@ -1,4 +1,37 @@
+function facebookShare(){
+
+    var getWindowOptions = function() {
+        var width = 500;
+        var height = 450;
+        var left = (window.innerWidth / 2) - (width / 2);
+        var top = (window.innerHeight / 2) - (height / 2);
+
+        return [
+        'resizable,scrollbars,status',
+        'height=' + height,
+        'width=' + width,
+        'left=' + left,
+        'top=' + top,
+        ].join();
+    };
+    var fbBtn = document.querySelector('.facebook');
+    var title = encodeURIComponent('Hey everyone, come & see how good I look!');
+    var shareUrl = 'https://www.facebook.com/sharer/sharer.php?u=' + location.href + '&title=' + title;
+    fbBtn.href = shareUrl; // 1
+
+    fbBtn.addEventListener('click', function(e) {
+      e.preventDefault();
+      var win = window.open(shareUrl, 'ShareOnFb', getWindowOptions());
+      win.opener = null; // 2
+    });
+
+
+}
+
+
 $(document).ready(function(){
+
+    facebookShare();
 
     $('.popup-close').click(function() {
             $('.popup-background').css('display', 'none');
