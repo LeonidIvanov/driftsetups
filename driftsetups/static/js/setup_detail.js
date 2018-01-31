94e8b32ec10dbe5c9f7ccbaa1a72c4181f78ea36
LeonidIvanov <git@github.com:LeonidIvanov/python0709.git>
2018-01-31 17:41:01 +0200

    Added image popoup for setup detail gallery

--- driftsetups/static/js/setup_detail.js
+++ driftsetups/static/js/setup_detail.js
@@ -92,7 +92,7 @@ function showSlides(n) {
 var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('sliderImages');
+var img = document.getElementById('sliderImage');
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
