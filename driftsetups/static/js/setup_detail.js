8f41f72a6a4c310c7b462cd770ecc34ec17c39ac
LeonidIvanov <git@github.com:LeonidIvanov/python0709.git>
2018-03-09 19:38:28 +0200

    Fixed setup detail image positioning

--- driftsetups/static/js/setup_detail.js
+++ driftsetups/static/js/setup_detail.js
@@ -98,14 +98,14 @@ function showSlides(n) {
 var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('sliderImage');
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
-img.onclick = function(){
+$(document).on('click', '.mySlides', function(){
+    console.log(this.src);
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
-};
+});
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
