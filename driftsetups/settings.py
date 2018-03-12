f8522daf02f7d0fd789f6ffecd20bda55804ecc5
LeonidIvanov <git@github.com:LeonidIvanov/python0709.git>
2018-03-12 14:38:39 +0200

    Added logging

--- driftsetups/settings.py
+++ driftsetups/settings.py
@@ -166,6 +166,24 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
+
+# Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
